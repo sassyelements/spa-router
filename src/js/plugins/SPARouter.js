933803dee3464c0a879cba19a84b02b81dd152c0
0xkhan <0xkhan@protonmail.com>
2023-03-15 17:51:29 +0100

    Add hash based routes support

--- src/js/plugins/SPARouter.js
+++ src/js/plugins/SPARouter.js
@@ -11,20 +11,47 @@ class SPARouter {
     setRoutes(routes) {
         Object.assign(this.#routes, routes);
         this.locationHandler();
+
+        return this;
+    }
+
+    initRouter({ selector }) {
+        if (this.#isRouteTypeHash(this.#routes)) {
+            document.addEventListener('hashChange', this.locationHandler);
+        } else {
+            document.addEventListener('click', (event) => {
+                event.preventDefault();
+                if (!event.target.matches(selector)) return;
+                this.route();
+            });
+        }
     }
 
     route(event) {
         event = event || window.event;
         event.preventDefault();
+
+        if (!this.#isValidPath(event.target.href)) {
+            window.open(event.target.href);
+            return;
+        }
+
         window.history.pushState({}, "", event.target.href);
         this.locationHandler();
     }
 
     async locationHandler() {
-        const location = window.location.pathname;
-        if (location.length == 0) {
-            location = '/';
+        let location;
+
+        if (this.#isRouteTypeHash(this.#routes)) {
+            // If routes are based on hash
+            location = window.location.hash;
+        } else {
+            // If routes are based on pathnames
+            location = window.location.pathname;
         }
+
+        if (location.length == 0) location = '/';
 
         const route = this.#routes[location] || this.#routes[404];
         const html = await fetch(route.template)
@@ -34,6 +61,23 @@ class SPARouter {
         document.title = route.title;
         document.querySelector('meta[name="description"]')
             .setAttribute("content", route.description);
+    }
+
+    #isValidPath(path) {
+        return path.includes(window.location.origin);
+    }
+
+    #isRouteTypeHash(routes) {
+        const allPaths = Object.keys(routes);
+        const actualPaths = [];
+
+        allPaths.forEach((path) => {
+            if (path !== '404' && path !== '/') {
+                actualPaths.push(path);
+            }
+        })
+
+        return actualPaths[0].startsWith('#');
     }
 
 }
