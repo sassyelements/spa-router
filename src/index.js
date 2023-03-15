a53069f16b424a70c99085b1cfbed5ef0b01304b
0xkhan <0xkhan@protonmail.com>
2023-03-15 17:50:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import router from './js/plugins/SPARouter';
 
 import './js/tests/testsRunner.js';
 
-
-router.setRoutes({
+const urlRoutes = {
     404: {
         template: './templates/404.html',
         title: '404 Not Found',
@@ -24,11 +23,28 @@ router.setRoutes({
         title: 'Contact',
         description: 'This is the contact page'
     }
-});
+}
+const hashRoutes = {
+    404: {
+        template: './templates/404.html',
+        title: '404 Not Found',
+        description: 'This is the 404 page. It loads when a page does not exist'
+    },
+    '/': {
+        template: './templates/home.html',
+        title: 'Home Page',
+        description: 'This is the home page'
+    },
+    '#about': {
+        template: './templates/about.html',
+        title: 'About',
+        description: 'This is the about page'
+    },
+    '#contact': {
+        template: './templates/contact.html',
+        title: 'Contact',
+        description: 'This is the contact page'
+    }
+}
 
-document.addEventListener('click', (event) => {
-    event.preventDefault();
-    if (!event.target.matches('nav a')) return;
-    router.route();
-});
-
+router.setRoutes(hashRoutes).initRouter({ selector: null });
